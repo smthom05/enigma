6e2ecd1ddc88f85acd529b3da5c26a68358a2c07
Scott Thomas <smthom05@gmail.com>
2019-01-04 16:19:12 -0700

    test it exists for enigma

---
+++ test/enigma_test.rb
@@ -0,0 +1,14 @@
+require './test/test_helper'
+
+class EnigmaTest < Minitest::Test
+  def test_it_exists
+    enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
+  end
+
+  def test_it_can_encrypt_messages
+    enigma = Enigma.new
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,9 @@
 require 'simplecov'
 SimpleCov.start
+
+# Minitest
+require 'minitest/autorun'
+require 'minitest/pride'
+
+# Classes and Modules
+require './lib/enigma'
