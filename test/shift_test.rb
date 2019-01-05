7cc3fa1933e4c914a02a28cd23acdb9afd597476
Scott Thomas <smthom05@gmail.com>
2019-01-05 15:33:00 -0700

    generate shift hash test passing.

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -12,9 +12,25 @@ class ShiftTest < Minitest::Test
 
     assert_equal ({}), shift.shift_hash
   end
-  # def test_it_can_generate_shift_hash
-  #   shift = Shift.new
-  #
-  #   assert
-  # end
+
+  def test_it_can_generate_shift_hash
+    shift = Shift.new
+    key = Key.new
+    offset = Offset.new
+
+    key_hash = key.generate_key_hash("02715")
+    offset_hash = offset.generate_offset_hash("040895")
+
+
+    shift.generate_shift_hash(key_hash, offset_hash)
+
+    expected = {
+      a: 3,
+      b: 27,
+      c: 73,
+      d: 20
+    }
+
+    assert_equal expected, shift.shift_hash
+  end
 end
