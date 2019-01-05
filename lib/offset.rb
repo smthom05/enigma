de60b77e9ed217ead9dd7a47d2749c27b9fb7224
Scott Thomas <smthom05@gmail.com>
2019-01-05 14:02:27 -0700

    test it can generate an offset hash given a date test built. Also made a small fix to KeyTest.

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,5 +1,23 @@
+require 'date'
+
 class Offset
+
+  attr_reader :offset_hash
+
   def initialize
+    @offset_hash = {
+      a: 0,
+      b: 0,
+      c: 0,
+      d: 0
+    }
   end
-  
+
+  def generate_offset
+    offset = Date.today.strftime("%d%m%y")
+  end
+
+  def generate_offset_hash(date = offset.generate_offset)
+
+  end
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -7,6 +7,12 @@ class OffsetTest < Minitest::Test
     assert_instance_of Offset, offset
   end
 
+  def test_it_starts_with_hash_values_of_0
+    offset = Offset.new
+
+    assert_equal 0, offset.offset_hash.values.first
+  end
+
   def test_it_can_generate_an_offset_using_todays_date
     offset = Offset.new
 
@@ -14,7 +20,26 @@ class OffsetTest < Minitest::Test
 
     expected = Date.today.strftime("%d%m%y")
 
-    assert_instance_of Offset, actual
+    assert_instance_of String, actual
     assert_equal expected, actual
+  end
+
+  def test_it_can_generate_an_offset_hash_given_a_date
+    offset = Offset.new
+
+    actual = offset.generate_offset("040895")
+
+    expected = {
+      a: 1,
+      b: 0,
+      c: 2,
+      d: 5
+    }
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_generate_an_offset_hash_using_todays_date
+
   end
 end
