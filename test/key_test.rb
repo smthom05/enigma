f3355117955c33b5da8e0a52c9c3c57f41f37e7b
Scott Thomas <smthom05@gmail.com>
2019-01-04 18:41:49 -0700

    Created tests and method for generating a random key if it's not given

--- test/key_test.rb
+++ test/key_test.rb
@@ -6,4 +6,13 @@ class KeyTest < Minitest::Test
 
     assert_instance_of Key, key
   end
+
+  def test_it_can_generate_a_random_key
+    key = Key.new
+
+    actual = key.generate_random_key
+
+    assert_instance_of String, actual
+    assert_equal 5, actual.length
+  end
 end
