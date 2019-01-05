0728fdb14afe7c156f42a087a08d5e982cc127e2
Scott Thomas <smthom05@gmail.com>
2019-01-04 18:07:42 -0700

    created test_it_has_the_desired_character_set

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -7,6 +7,14 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, enigma
   end
 
+  def test_it_has_the_desired_character_set
+    enigma = Enigma.new
+
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, enigma.character_set
+  end
+
   def test_it_can_encrypt_messages_with_key_and_date_given
     enigma = Enigma.new
 
