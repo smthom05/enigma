004c339f37fe7080417a8b90e55836c8c30ce5c8
Scott Thomas <smthom05@gmail.com>
2019-01-05 13:51:17 -0700

    test for generating offset using today's date complete

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -6,4 +6,15 @@ class OffsetTest < Minitest::Test
 
     assert_instance_of Offset, offset
   end
+
+  def test_it_can_generate_an_offset_using_todays_date
+    offset = Offset.new
+
+    actual = offset.generate_offset
+
+    expected = Date.today.strftime("%d%m%y")
+
+    assert_instance_of Offset, actual
+    assert_equal expected, actual
+  end
 end
