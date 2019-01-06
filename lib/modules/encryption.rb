5e40294bcbeee31cec0b6c6ba7a686a85f22a69a
Scott Thomas <smthom05@gmail.com>
2019-01-06 13:42:34 -0700

    added functionality to create a hash with all of the possible character sets in it

--- lib/modules/encryption.rb
+++ lib/modules/encryption.rb
@@ -2,16 +2,18 @@ module Encryption
 
   def encrypt_message(message, key, date)
     encrypted_word = ""
-    shift = gather_hashes(key,date)
+    new_character_set_hash = gather_hashes(key,date)
     letters_hash = group_letters_by_shift(message)
     binding.pry
-  
+
+
   end
 
   def gather_hashes(key, date)
     key = Key.new.generate_key_hash(key)
     offset = Offset.new.generate_offset_hash(date)
     shift = Shift.new.generate_shift_hash(key, offset)
+    new_character_set_hash = generate_new_character_sets(shift)
   end
 
   def group_letters_by_shift(message)
@@ -19,20 +21,29 @@ module Encryption
     i = 0
     message.each_char do |char|
       if i == 0
-        letters_hash[i] += char
+        letters_hash[:a] += char
         i += 1
       elsif i == 1
-        letters_hash[i] += char
+        letters_hash[:b] += char
         i += 1
       elsif i == 2
-        letters_hash[i] += char
+        letters_hash[:c] += char
         i += 1
       elsif i == 3
-        letters_hash[i] += char
+        letters_hash[:d] += char
         i = 0
       end
     end
     letters_hash
   end
+
+  def generate_new_character_sets(shift)
+    character_set_hash = {}
+    shift.each do |k,v|
+      character_set_hash[v] = character_set.rotate(v)
+    end
+    character_set_hash
+  end
+
 
 end
