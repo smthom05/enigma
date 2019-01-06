b4ca178838d32bb1b8edbb6803c410083afd6942
Scott Thomas <smthom05@gmail.com>
2019-01-06 14:56:42 -0700

    encypt method in the module seems to be encrypting as expected. It is a little long still so refactors will need to be made.

--- lib/modules/encryption.rb
+++ lib/modules/encryption.rb
@@ -1,10 +1,27 @@
 module Encryption
 
   def encrypt_message(message, key, date)
-    encrypted_word = ""
     new_character_sets_hash = gather_hashes(key,date)
-    letters_hash = group_letters_by_shift(message)
+    encrypted_word = ""
 
+    i = 0
+    message.each_char do |char|
+      index = character_set.index(char)
+      if i == 0
+        encrypted_word += new_character_sets_hash[:a].values_at(index).pop
+        i += 1
+      elsif i == 1
+        encrypted_word += new_character_sets_hash[:b].values_at(index).pop
+        i += 1
+      elsif i == 2
+        encrypted_word += new_character_sets_hash[:c].values_at(index).pop
+        i += 1
+      elsif i == 3
+        encrypted_word += new_character_sets_hash[:d].values_at(index).pop
+        i = 0
+      end
+      encrypted_word
+    end
   end
 
   def gather_hashes(key, date)
@@ -14,45 +31,12 @@ module Encryption
     new_character_sets_hash = generate_new_character_sets(shift)
   end
 
-  # def group_letters_by_shift(message)
-  #   letters_hash = Hash.new("")
-  #   i = 0
-  #   message.each_char do |char|
-  #     if i == 0
-  #       letters_hash[:a] += char
-  #       i += 1
-  #     elsif i == 1
-  #       letters_hash[:b] += char
-  #       i += 1
-  #     elsif i == 2
-  #       letters_hash[:c] += char
-  #       i += 1
-  #     elsif i == 3
-  #       letters_hash[:d] += char
-  #       i = 0
-  #     end
-  #   end
-  #   letters_hash
-  # end
-
-  def group_letters_by_shift(message)
-    message_arr = message.downcase.each_char.map {|char| char}
-
-    letters_hash = {}
-    letters_hash[:a] = message_arr.values_at(0,4,8,12).map
-    letters_hash[:a] = message_arr.values_at(1,5,9,13).map
-    letters_hash[:a] = message_arr.values_at(2,6,10,14).map
-    letters_hash[:a] = message_arr.values_at(3,7,11,15).map
-    letters_hash
-  end
-
   def generate_new_character_sets(shift)
     character_sets_hash = {}
     shift.each do |k,v|
-      character_sets_hash[v] = character_set.rotate(v)
+      character_sets_hash[k] = character_set.rotate(v)
     end
     character_sets_hash
   end
-
 
 end
