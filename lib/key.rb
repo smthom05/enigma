c18a036d182075777ab2a457dc1727f225aa9e07
Scott Thomas <smthom05@gmail.com>
2019-01-07 14:37:12 -0700

    made some minor refactors to reduce a line here and there

--- lib/key.rb
+++ lib/key.rb
@@ -7,8 +7,7 @@ class Key
   end
 
   def generate_random_key
-    key_range = ("00001".."99999")
-    key = key_range.to_a.sample
+    key = ("00001".."99999").to_a.sample
     if key.length != 5
       key.rjust(5,'0')
     end
